85ac23526da2e89299d7c3b3af340a920978e426
qyysrk <qyysrk@gmail.com>
2020-04-23 13:43:06 +0700

    upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
-import ColorBox from './components';
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: 'Xin chao cac ban' },
+    { id: 2, title: 'Xin chao cac ban hi ' },
+    { id: 3, title: 'Xin chao cac ban hihi' },
+  ]);
+
+  function handleTodoClick(todo) {
+    console.log(todo);
+    const index = todoList.findIndex(x => x.id === todo.id);
+    if (index < 0) return;
+
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+
+
+  }
+
+  function handleTodoFormSubmit(formValues) {
+    console.log('Form submit: ', formValues);
+    //add new todo to current todo list
+
+    const newTodo = {
+      id: todoList.length + 1,
+      ...formValues,
+    };
+
+    const newTodoList = [...todoList];
+    newTodoList.push(newTodo);
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="app">
-      <h1> Welcome to react-hook </h1>
-      <ColorBox />
+      <h1> Welcome to react-hook  and todoList </h1>
+      {/* <ColorBox />
+       */}
+      <TodoForm onSubmit={handleTodoFormSubmit} />
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
 }
